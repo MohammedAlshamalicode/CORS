386b74c26782f8a3757f163766f1b6d89a6d467b
MohammedAlshamalicode <mshamaly0023@gmail.com>
2025-03-18 11:45:37 +0100

    CORS

---
+++ src/main/java/be/vdab/cors/KwadraatController.java
@@ -0,0 +1,13 @@
+package be.vdab.cors;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("kwadraat")
+class KwadraatController {
+    @GetMapping("{getal}")
+    int kwadraat(@PathVariable int getal) {
+        return getal * getal;
+    }
+}
